d3c88295bc640d12c6903cbcf75b14ea388de32b
Martin Quinson <martin.quinson@ens-rennes.fr>
2018-11-09 08:37:38 +0100

    MapReduce: enforce the modulo 3 per card instead of per page
    Some cards may get lost, defeating our verification.
    
    To make it work, I had to go from the rule XX % 3 == 0 to XX % 3 == 1,
    and remove one pattern.
    
    I tried to play alone, and 4 patterns seem better to me anyway.
    
    I also changed the generation algorithm so that the patterns counts
    are not always the same on each card. The way I'm doing for skewed is
    .. somewhat cheated but I dunno how to do it properly.

--- pedago2/2018-MapReduce/generate.py
+++ pedago2/2018-MapReduce/generate.py
@@ -4,7 +4,7 @@ import os
 import re
 import random
 
-patterns = ['bicycle', 'fish', 'rocket', 'tree', 'walking']#, 'cat', 'seedling', 'trophy' , 'crow', 'truck']
+patterns = ['bicycle',  'rocket', 'tree', 'walking']#'fish',, 'cat', 'seedling', 'trophy' , 'crow', 'truck']
 
 pmargin=1200 # page margin, in the unit of the viewBox
 cmargin=645 # margin within the card
@@ -38,38 +38,49 @@ for filename in patterns:
     
     ypad[filename] = (cellsize - int(box.group(2))) / 2
     assert ypad[filename] > 0, "the cell is too small for {:s}.svg: cellsize is {:d} but image is x{:s} y{:s}".format(filename, cellsize, box.group(1), box.group(2))
-
+ 
 # Generate a distribution of icons, skewed or not
-# The number of each pattern in one page will always be a multiple of 3
-assert (cardsize * cardsize * xcard * ycard) % 3 == 0, "the total number of icons in the page {:d} is not a multiple of 3".format(cardsize * cardsize * xcard * ycard)
-total = int((cardsize * cardsize * xcard * ycard) / 3)
-if skew:
-  amounts = [0 for i in range(len(patterns))]
-  cursum = 0
-  for i in range(len(amounts) - 1):
-    x = random.randint(1, total - cursum - (len(amounts) - i))
-    cursum += x
-    amounts[i] = x
-  amounts[-1] = total - cursum
-else:
-  amounts = [int(total / len(patterns)) for i in range(len(patterns))]
-  amounts[-1] += total - sum(amounts)
-amounts = [3 * e for e in amounts]
-assert sum(amounts) == (total * 3), "thid should not happen"
-print(amounts, sum(amounts))
+# The number of each pattern in one batch will always be so that XX % 3 == 1
+def generate_amounts(target_size):
+    assert (target_size - len(patterns)) % 3 == 0, "the batch size {:d} modulo 3 is not 1".format(target_size)
+    total = int((target_size - len(patterns)) / 3)
+    amounts_res = [0 for i in range(len(patterns))]
+    if skew:
+        for i in range(total):
+            x = random.randint(0, len(patterns)+1)
+            if x>len(patterns)-1: # There will be some more pattern=0
+                x=0
+            amounts_res[x] += 1
+#        cursum = 0
+#        for i in range(len(amounts_res) - 1):
+#            x = random.randint(0, total - cursum - (len(amounts_res) - i))
+#            cursum += x
+#            amounts_res[i] = x
+#            amounts_res[-1] = total - cursum
+    else:
+        for i in range(total):
+            x = random.randint(0, len(patterns)-1)
+            amounts_res[x] += 1
+    amounts_res = [3 * e +1 for e in amounts_res]
+    assert sum(amounts_res) == target_size, "This should not happen: {:d} != {:d}".format(sum(amounts_res), target_size)
+    print(amounts_res, sum(amounts_res))
+    return amounts_res
 
 # Compute the data content
 # Generation is done using the distribution computed above
-data = [[0 for j in range(cardsize * ycard)] for i in range(cardsize * xcard)]
-pool = [i for i in range(len(patterns))]
-curamounts = [0 for i in range(len(amounts))]
-for x in range(cardsize * xcard):
-    for y in range(cardsize * ycard):
-        p = random.choice(pool)
-        curamounts[p] += 1
-        if curamounts[p] >= amounts[p]:
-          del pool[pool.index(p)]
-        data[x][y] = p
+data = [[-1 for j in range(cardsize * ycard)] for i in range(cardsize * xcard)]
+for xc in range(xcard):
+    for yc in range(ycard):
+        amounts = generate_amounts(cardsize * cardsize)
+        pool = [i for i in range(len(patterns))]
+        curamounts = [0 for i in range(len(amounts))]
+        for x in range(cardsize):
+            for y in range(cardsize):
+                p = random.choice(pool)
+                curamounts[p] += 1
+                if curamounts[p] >= amounts[p]:
+                    del pool[pool.index(p)]
+                data[x+xc*cardsize][y+yc*cardsize] = p
     
 # Helping function
 def cell_to_viewport(pattern, x, y):
